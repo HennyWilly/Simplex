0bb673dd1625a6d416804a675d97bcbe4da598fe
Karsten Goebbels <karsten-g@gmx.de>
2016-11-30 11:05:47 +0100

    Fixed unittest relative path

--- pythonTest/test_FileHandle.py
+++ pythonTest/test_FileHandle.py
@@ -1,5 +1,5 @@
+import os
 import pytest
-
 import env
 from python.FileHandle import FileHandle
 
@@ -7,14 +7,17 @@ from python.FileHandle import FileHandle
 def test_shouldGetInputFiles():
     # This is a bad test, but I don't know how to test this function better...
 
-    fileHandle = FileHandle("../input")
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    path = os.path.join(dir_path, '..', 'input')
+    fileHandle = FileHandle(path)
     files = fileHandle.getInputFiles()
     assert len(files) == 4
 
 
 def test_shouldParseFile():
-    file = "../input/Exercise01.txt"
-    lps = FileHandle.parseInputFile(file)
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    path = os.path.join(dir_path, '..', 'input', 'Exercise01.txt')
+    lps = FileHandle.parseInputFile(path)
 
     # We don't need to check the lps here, because this is the job of "test_InputParser.py"
     assert len(lps) == 2
